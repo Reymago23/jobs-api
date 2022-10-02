5c8226da9076a084189a0318acad39a65696ef5c
Reymago23 <elderriqui@gmail.com>
2022-10-01 18:48:54 -0600

    improve error messages

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,10 +1,20 @@
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
+
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || 'Something went wront, try again later.'
   }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  if (err.name == 'ValidationError') {
+    customError.msg = Object.values(err.errors).map(item => item.message).join(', ')
+    customError.statusCode = StatusCodes.BAD_REQUEST
+  }
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value for ${Object.keys(err.keyValue)}, please choose another value and try again.`
+    customError.statusCode = StatusCodes.BAD_REQUEST
+  }
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg: customError.msg })
 }
 
 module.exports = errorHandlerMiddleware
